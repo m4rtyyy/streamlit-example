756469a5d135117caa8cc2530bc80a0ed0ac7220
m4rtyyy <126189241+m4rtyyy@users.noreply.github.com>
2023-02-23 18:48:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import subprocess
 
 def open_terminal():
-    subprocess.Popen('x-terminal-emulator')
+    subprocess.Popen(['gnome-terminal'])
     
 if st.button('Open Terminal'):
     open_terminal()
