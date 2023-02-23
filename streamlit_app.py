3621070b4c9f736c7f4f2a56e424ee5f54dd2621
m4rtyyy <126189241+m4rtyyy@users.noreply.github.com>
2023-02-23 18:47:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,8 @@
-import os
-
-if 'streamlit run' in os.sys.argv[0]:
-    os.system('streamlit run --server.port $PORT --server.address 0.0.0.0 --browser.gatherUsageStats false python.py &>/dev/null &')
-
-
-import openai
 import streamlit as st
+import subprocess
 
-openai.api_key = "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"
-
-def main():
-    st.title("Virtual Assistant")
-    st.write("I am a virtual assistant powered by OpenAI's GPT-3 language model. Ask me anything!")
+def open_terminal():
+    subprocess.Popen('x-terminal-emulator')
     
-    user_input = st.text_input("You: ")
-    
-    if user_input:
-        response = generate_response(user_input)
-        st.write("Virtual Assistant: " + response)
-
-def generate_response(input_text):
-    prompt = f"Conversation with a user:\nUser: {input_text}\nVirtual Assistant:"
-    completions = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-
-    message = completions.choices[0].text.strip()
-    return message
-
-if __name__ == "__main__":
-    main()
+if st.button('Open Terminal'):
+    open_terminal()
