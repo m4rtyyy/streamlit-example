60f682431e9a64e215c1e65d926cce374d588d05
m4rtyyy <126189241+m4rtyyy@users.noreply.github.com>
2023-02-23 18:20:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,39 @@
 import streamlit as st
+import datetime
 
 def main():
-    st.title("Text Generator")
-    
-    text_input = st.text_input("Enter some text:")
-    
-    if text_input:
-        generated_text = generate_text(text_input)
-        st.write(generated_text)
+    st.title("Virtual Assistant")
 
-def generate_text(input_text):
-    # This is a simple example that just repeats the input text
-    generated_text = input_text + " " + input_text
-    return generated_text
+    user_input = st.text_input("How can I help you?")
+
+    if user_input:
+        response = generate_response(user_input)
+        st.write(response)
+
+def generate_response(input_text):
+    if "time" in input_text:
+        now = datetime.datetime.now()
+        response = f"The current time is {now.strftime('%H:%M')}."
+    
+    elif "date" in input_text:
+        now = datetime.datetime.now()
+        response = f"Today is {now.strftime('%A, %B %d, %Y')}."
+    
+    elif "weather" in input_text:
+        response = "Sorry, I'm not able to provide weather information at the moment."
+    
+    elif "calculate" in input_text:
+        expression = input_text.split("calculate ")[-1]
+        try:
+            result = eval(expression)
+            response = f"The result of {expression} is {result}."
+        except:
+            response = "Sorry, I wasn't able to perform that calculation."
+    
+    else:
+        response = "Sorry, I didn't understand your request."
+
+    return response
 
 if __name__ == "__main__":
     main()
