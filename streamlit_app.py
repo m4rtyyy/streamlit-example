4a5d17c1fa3e42acd2773e3460d99a00f1984751
m4rtyyy <126189241+m4rtyyy@users.noreply.github.com>
2023-02-23 18:29:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,22 @@
 import openai
 import streamlit as st
 
-openai.api_key = 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
+openai.api_key = "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"
 
 def main():
-    st.title("ChatGPT")
+    st.title("Virtual Assistant")
+    st.write("I am a virtual assistant powered by OpenAI's GPT-3 language model. Ask me anything!")
     
     user_input = st.text_input("You: ")
     
     if user_input:
         response = generate_response(user_input)
-        st.write("ChatGPT: " + response)
+        st.write("Virtual Assistant: " + response)
 
 def generate_response(input_text):
-    prompt = f"Conversation with a user:\nUser: {input_text}\nChatGPT:"
+    prompt = f"Conversation with a user:\nUser: {input_text}\nVirtual Assistant:"
     completions = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="davinci",
         prompt=prompt,
         max_tokens=1024,
         n=1,
