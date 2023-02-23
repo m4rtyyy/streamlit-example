776de140388fa602b1761d57ec3d56f2bfe8e6a4
m4rtyyy <126189241+m4rtyyy@users.noreply.github.com>
2023-02-23 18:17:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,18 @@
 import streamlit as st
-import spacy
-
-nlp = spacy.load("en_core_web_sm")
 
 def main():
-    st.title("ChatGPT - Your Personal AI Assistant")
-    user_input = st.text_input("Ask me a question")
+    st.title("Text Generator")
     
-    if user_input:
-        doc = nlp(user_input)
-        subject = None
-        for token in doc:
-            if token.dep_ == "nsubj":
-                subject = token.text
-                break
-        
-        if subject:
-            response = f"I'm sorry, I don't know much about {subject}."
-            # You can add your own logic here to generate a response based on the subject
-            
-        else:
-            response = "I'm sorry, I didn't understand your question."
-        
-        st.write(response)
+    text_input = st.text_input("Enter some text:")
+    
+    if text_input:
+        generated_text = generate_text(text_input)
+        st.write(generated_text)
+
+def generate_text(input_text):
+    # This is a simple example that just repeats the input text
+    generated_text = input_text + " " + input_text
+    return generated_text
 
 if __name__ == "__main__":
     main()
